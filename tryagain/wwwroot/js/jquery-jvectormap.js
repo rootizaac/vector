3de4af85966f53809a8afe6daa17383fc1fea679
18186 <18186@DESKTOP-jwzqZS>
2023-08-16 01:20:17 -0700

    Add project files.

---
+++ tryagain/wwwroot/js/jquery-jvectormap.js
@@ -0,0 +1,57 @@
+/**
+ * jVectorMap version 2.0.5
+ *
+ * Copyright 2011-2014, Kirill Lebedev
+ *
+ */
+
+(function ($) {
+    var apiParams = {
+        set: {
+            colors: 1,
+            values: 1,
+            backgroundColor: 1,
+            scaleColors: 1,
+            normalizeFunction: 1,
+            focus: 1
+        },
+        get: {
+            selectedRegions: 1,
+            selectedMarkers: 1,
+            mapObject: 1,
+            regionName: 1
+        }
+    };
+
+    // Define jvm.Map if it doesn't exist
+    if (typeof jvm === 'undefined') {
+        window.jvm = {};
+    }
+
+    jvm.Map = function (options) {
+        // constructor body
+        // Example: this.options = options;
+    };
+
+    $.fn.vectorMap = function (options) {
+        var map, methodName;
+        map = this.children('.jvectormap-container').data('mapObject');
+
+        if (options === 'addMap') {
+            if (typeof jvm.Map.maps === 'undefined') {
+                jvm.Map.maps = {};
+            }
+            jvm.Map.maps[arguments[1]] = arguments[2];
+        } else if ((options === 'set' || options === 'get') && apiParams[options][arguments[1]]) {
+            methodName = arguments[1].charAt(0).toUpperCase() + arguments[1].substr(1);
+            return map[options + methodName].apply(map, Array.prototype.slice.call(arguments, 2));
+        } else {
+            options = options || {};
+            options.container = this;
+            map = new jvm.Map(options);
+        }
+
+        return this;
+    };
+
+})(jQuery);
